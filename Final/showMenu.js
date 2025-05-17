ce0b9bbff2c53ebe621be5fa2a294fb657227ced
ZStuden <zen.sabio@stu.bmcc.cuny.edu>
2025-05-17 18:33:10 +0000

    added animations and added in better style toggle

--- Final/showMenu.js
+++ Final/showMenu.js
@@ -1,17 +1,28 @@
-function showMenu(){
+/* function showMenu(){
         var  menu = document.getElementById("topmenu");
         if  (menu.style.display==="none" || menu.style.display === ''){
             menu.style.display="block";
         } else {
         menu.style.display="none";
             }
-        }
+        }  */
 
-function toggleDropper(id){
+function showMenu(){
+    const menu = document.getElementById("topmenu");
+    menu.classList.toggle("open");
+} 
+
+/* function toggleDropper(id){
     var dropper = document.getElementById(id);
     if (dropper.style.display === "none" || dropper.style.display ==="") {
         dropper.style.display = "block";
     } else {
         dropper.style.display ="none";
     }
+} */
+
+function toggleDropper(id){
+    var dropper = document.getElementById(id);
+    dropper.classList.toggle("open");
 }
+
